610ab19c3df84587ab71bf2808ddee77e6054d00
SonyaOrlova <sonya5488@yandex.ru>
2020-06-16 20:05:14 +0300

    Миграция ограничения целостности сущностей.

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_164724) do
+ActiveRecord::Schema.define(version: 2020_06_16_165046) do
 
   create_table "answers", force: :cascade do |t|
-    t.string "body"
-    t.boolean "is_correct"
-    t.integer "question_id"
+    t.string "body", null: false
+    t.boolean "is_correct", null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.integer "test_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -36,18 +36,18 @@ ActiveRecord::Schema.define(version: 2020_06_16_164724) do
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
-    t.integer "level"
-    t.integer "category_id"
+    t.string "title", null: false
+    t.integer "level", null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
