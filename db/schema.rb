ecb23fb44ac50dff57322ce9b7015112e76c6ff7
SonyaOrlova <sonya5488@yandex.ru>
2020-06-18 20:43:53 +0300

    Миграция целостности сущностей для атрибута test_id модели question.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_170600) do
+ActiveRecord::Schema.define(version: 2020_06_18_174054) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_06_16_170600) do
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "test_id"
+    t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
