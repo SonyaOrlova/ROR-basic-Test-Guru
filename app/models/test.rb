5b5df6dcb0d65223882c49ebe1cd2689f9520e08
SonyaOrlova <sonya5488@yandex.ru>
2020-06-19 13:03:31 +0300

    Добавление метода desc_tests_titles_by_category_title для модели Test.

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,4 +4,8 @@ class Test < ApplicationRecord
   has_many :questions
 
   has_and_belongs_to_many :users
+
+  def self.desc_tests_titles_by_category_title(category_title)
+    Test.where(category_id: Category.where(title: category_title)).order(title: :desc).pluck(:title)
+  end
 end
