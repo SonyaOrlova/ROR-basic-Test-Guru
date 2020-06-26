7db20e5fac96a513c42c7f4bc889d7b809f44af0
SonyaOrlova <sonya5488@yandex.ru>
2020-06-26 17:47:40 +0300

    Добавление контроллера для ресурса вопросов.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'tests#index'
+
+  resources :tests, only: [:index] do
+    resources :questions, only: [:index, :show, :new, :create, :destroy], shallow: true
+  end
 end
