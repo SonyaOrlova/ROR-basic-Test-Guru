4a09ca7701439971f3e6473139028af40ceb9ae5
SonyaOrlova <sonya5488@yandex.ru>
2020-06-30 21:29:09 +0300

    Добавление методов для интерфейса тест-вопросы.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,3 +1,15 @@
 class TestsController < ApplicationController
-  def index; end
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  def rescue_with_test_not_found
+    redirect_to root_path
+  end
 end
