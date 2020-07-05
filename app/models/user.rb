9d4307790b52ac14cebb69724843363e5a8bd29f
SonyaOrlova <sonya5488@yandex.ru>
2020-07-05 20:32:44 +0300

    Создание таблицы TestPassages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
 
-  has_and_belongs_to_many :passed_tests, class_name: 'Test', foreign_key: 'test_id'
+  has_many :test_passages
+  has_many :passed_tests, through: :test_passages, source: :test
 
   def tests_by_level(level)
     tests.where(level: level)
