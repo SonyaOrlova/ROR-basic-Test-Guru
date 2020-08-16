3eaee7b9dc54975ab2e6e30407130a6b351a03e0
SonyaOrlova <sonya5488@yandex.ru>
2020-08-16 18:39:16 +0300

    Добавление проверки совпадения пароля и подтверждения.

---
+++ app/javascript/utilities/password-confirmation.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const passwordInputEl = document.querySelector('#user_password');
+  const passwordConfirmationInputEl = document.querySelector('#user_password_confirmation');
+
+  if (!passwordConfirmationInputEl) return;
+
+  passwordConfirmationInputEl.addEventListener('input', () => {
+    if (!passwordConfirmationInputEl.value) {
+      passwordConfirmationInputEl.classList.remove('valid', 'invalid');
+      return;
+    }
+
+    if (passwordConfirmationInputEl.value !== passwordInputEl.value) {
+      passwordConfirmationInputEl.classList.remove('valid');
+      passwordConfirmationInputEl.classList.add('invalid');
+    } else {
+      passwordConfirmationInputEl.classList.remove('invalid');
+      passwordConfirmationInputEl.classList.add('valid');
+    }
+  });
+});
